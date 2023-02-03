7169d6ac60aee746f0261e287f5991b597bb70b7
nvencer98 <vencer98@gmail.com>
2023-02-03 10:35:49 -0800

    corrected testing error

--- src/pages/CharacterShow.test.js
+++ src/pages/CharacterShow.test.js
@@ -1,7 +1,31 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import CharacterShow from "./CharacterShow"
+import mockCharacters from "../mockCharacters"
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+
+
+
+const renderShow = () => {
+    render(
+    <MemoryRouter initialEntries={["/charactershow/1"]}>
+      <Routes>
+      <Route path="charactershow/:id" element={<CharacterShow characters={mockCharacters}/>} />
+      </Routes>
+    </MemoryRouter>
+    )
+  }
+
 
 
 describe("<CharacterShow />", () => {
-  it("renders without crashing", () => {})
-})
+
+
+  it("renders a card without crashing", () => {
+    const div = document.createElement("div")
+    renderShow()
+    screen.logTestingPlaygroundURL()
+    expect(screen.getByRole('img', { name: /profile of a character named spider\-man/i })).toBeInTheDocument()
+    })
+  })
+
+  
